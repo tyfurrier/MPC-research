4fe76acfd8c352572dcedca3c44b7278d6b3bfa8
tyfurrier <tyler.furrier@atscale.com>
2023-11-08 17:50:03 -0500

    checkpoint for pc

--- interface.py
+++ interface.py
@@ -1,7 +1,13 @@
 import tkinter as tk
+from playsound import playsound
+import numpy as np
+from scipy.io.wavfile import write
+from scipy import signal
+import matplotlib.pyplot as plt
+
 
 def violin_sound(window: int,
-                 formant_width: int,
+                 formant_width: int = 100,
                  fundamental: int = 440):
     """ Plays a violin sound with the duration of the given window size in milliseconds.
     Args:
@@ -12,4 +18,34 @@ def violin_sound(window: int,
         in between.
         fundamental (int): The fundamental frequency in Hz. Defaults to 440.
         """
+    db_levels = [60, 50, 45, 50, 43, 48, 37, 48, 30, 20]
+    equation = """
+    db = 20log(y)
+    db = log(y^20)
+    10^db = y^20
+    10^(db/20) = y"""
+    to_magnitude = lambda db: 10 ** (db / 20)
+    partials_magnitude = [to_magnitude(db) for db in db_levels]
+    partials_magnitude = db_levels
+    bitrate = 44100
+    master_vol = 0.0000001
+    freq = 440
+    length = window // 1000
 
+    # create time values
+    t = np.linspace(0, length, length * bitrate, dtype=np.float32)
+    # generate y values for signal
+    y = partials_magnitude[0] * master_vol * np.sin(2 * np.pi * fundamental * t)
+    for harmonic, level in enumerate(partials_magnitude[1:]):
+        harmonic += 1
+        frequency = fundamental * harmonic + fundamental
+        y += level * master_vol * np.sin(2 * np.pi * frequency * t)
+        print(f"Harmonic {harmonic} at {frequency} Hz with magnitude {level}")
+
+    # save to wave file
+    write("violin.wav", bitrate, y)
+
+
+
+if __name__ == "__main__":
+    violin_sound(window=3000)
