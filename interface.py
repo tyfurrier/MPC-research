ea2748cbd82b995c56eda4265376b42cd722bd5a
tyfurrier <tyler.furrier@atscale.com>
2023-11-30 14:20:38 -0500

    checkpoint

--- interface.py
+++ interface.py
@@ -13,23 +13,34 @@ from scipy.io import wavfile
 from scipy import signal
 import matplotlib.pyplot as plt
 import os, shutil
-from typing import Tuple
-from enum import Enum
+from typing import Tuple, List, Dict
+from enums import Decomposition, SoundFileType
+from helpers import _custom_write, _prep_directory
 
 LOGGER = logging.Logger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-class Decomposition(Enum):
-    FULL = ""
-    OCTAVE = "octave_"
-    HOLLOW = "missing_octave_"
-    RECONSTRUCTED = "reconstructed_"
+OUTPUT_DIR = "created_samples"
 
-    def file_naming(self):
-        if self.value == "":
-            return "full"
-        else:
-            return self.value[:-1]
+def spread_file_naming(radius: float, part: Decomposition, ct_spacing: float = 0.1):
+    """ Returns the file name for the spread sound. """
+    spacing_key = "" if ct_spacing == 0.1 else f"_{str(ct_spacing).replace('.', '-')}gaps"
+    return f"{part.file_naming()}_{str(int(radius * 10))}_ct{spacing_key}.wav"
+
+
+def get_combination_naming(base_radius, octave_radius,
+                           ratio,
+                           ):
+    return f"base-{base_radius*10:03.0f}_octave-{octave_radius*10:03.0f}_ratio-{ratio*100}.wav"
+
+def get_spread_sound(folder_name: str,
+                     cents: float,
+                     part: Decomposition,) -> Tuple[np.ndarray, float]:
+    fname = os.path.join(OUTPUT_DIR, folder_name,
+                         spread_file_naming(radius=cents, part=part))
+    if not os.path.exists(fname):
+        raise Exception(f"File {fname}, {cents} radius, {part} does not exist")
+    return librosa.load(fname)
 
 def get_cached_wave(f: int, part: Decomposition,
                     cents: int = 0) -> Tuple[np.ndarray, float]:
@@ -41,8 +52,7 @@ def get_cached_wave(f: int, part: Decomposition,
         Tuple[np.ndarray, int]: The wave and the sample rate.
     """
     midi_number = librosa.hz_to_midi(f) + cents / 100
-    fname = os.path.join("created_samples", "cents", f"{str(midi_number).replace('.', 'p')}"
-                                            f"{part.file_naming()}.wav")
+    fname = os.path.join(OUTPUT_DIR, "cents440", f"{part.file_naming()}_{int(cents*10)}cz.wav")
     if not os.path.exists(fname):
         wave, sr = trumpet_sound(frequency=librosa.midi_to_hz(midi_number),
                                  bitrate=44100,
@@ -146,9 +156,7 @@ def trumpet_harmonic_decibels(part: Decomposition = Decomposition.FULL) -> list:
     if part == Decomposition.FULL:
         return decibels
     elif part == Decomposition.HOLLOW:
-        for i in range(1, len(decibels), 2):
-            decibels[i] = None
-        return decibels
+        return [decibels[i] if i % 2 == 0 else None for i in range(len(decibels))]
     elif part == Decomposition.OCTAVE:
         return [decibels[i] if i % 2 == 1 else None for i in range(len(decibels))]
     else:
@@ -243,7 +251,7 @@ def trumpet_missing_octave(frequency: int = 440, bitrate: int = 44100,
     if not np.allclose(reconstructed, trumpet):
         print(f"reconstructed and original are not close f: {frequency}")
     if file_path is None:
-        file_path = "created_samples"
+        file_path = OUTPUT_DIR
     if os.path.exists(file_path):
         if overwrite_folder:
             shutil.rmtree(file_path)
@@ -255,24 +263,26 @@ def trumpet_missing_octave(frequency: int = 440, bitrate: int = 44100,
                 f"{d_.file_naming()}.wav"
         write(os.path.join(file_path, fname), bitrate, wave)
 
-def create_radii(radius: int = 99, folder_path: str = None, override: bool = False):
+def create_radii(radius: int = 99,
+                 folder_path: str = None,
+                 overwrite: bool = False,
+                 clear_dir: bool = False,
+                 parts: List[Decomposition] = None,
+                 ):
     count = 1
-    p = Decomposition.FULL
-    for p in [
-        Decomposition.FULL,
-        # Decomposition.OCTAVE,
-        # Decomposition.HOLLOW
-              ]:
+    if clear_dir is True and overwrite is False:
+        raise ValueError("clear_dir is True and overwrite is False")
+    if parts is None:
+        parts = [Decomposition.FULL, Decomposition.OCTAVE, Decomposition.HOLLOW]
+    for p in parts:
         pure_synth, sr = get_cached_wave(f=440, cents=0, part=p)
         sum = pure_synth / count
-        if folder_path is None:
-            folder_path = os.path.join("created_samples", f"radius_{str(uuid.uuid1())}")
-        if os.path.exists(folder_path):
-            if override:
-                shutil.rmtree(folder_path)
-                os.mkdir(folder_path)
-            else:
-                raise FileExistsError(folder_path)
+
+        default_path = os.path.join(OUTPUT_DIR, f"radius_{str(uuid.uuid1())}")
+        _prep_directory(folder_path=folder_path,
+                        default_path=default_path,
+                        clear_dir=clear_dir)
+
         for i in range(1, radius * 10):
             logging.debug(f"radius {str(i / 1000)}")
             distance = i / 10
@@ -283,7 +293,46 @@ def create_radii(radius: int = 99, folder_path: str = None, override: bool = Fal
             right, _ = get_cached_wave(f=440, cents=-distance, part=p)
             sum += left / count
             sum += right / count
-            write(os.path.join(folder_path, f"{p.file_naming()}_{i}_ct.wav"), sr, sum)
+            file_name = f"{p.file_naming()}_{i}_ct.wav"
+            file_path = os.path.join(folder_path, file_name)
+            _custom_write(path=file_path, sr=sr, wave=sum, overwrite=overwrite)
+
+def make_combinations(
+    folder_path: str,
+    combinations: Dict[Decomposition, List[float]],
+    source_folder: str = "radius_50",
+    octave_portion: float = 0.5,
+    overwrite: bool = False,
+    clear_dir: bool = False,
+):
+    """ Makes combinations of various decompositions"""
+    if clear_dir is True and overwrite is False:
+        raise ValueError("clear_dir is True and overwrite is False")
+    _prep_directory(folder_path=folder_path,
+                    default_path=os.path.join(OUTPUT_DIR, f"combinations_{uuid.uuid1()}"),
+                    clear_dir=clear_dir)
+    for base_radius in combinations[Decomposition.FULL]:
+        base_hollow, sr = get_spread_sound(folder_name=source_folder,
+                                          cents=base_radius,
+                                          part=Decomposition.HOLLOW)
+        base_octave, _ = get_spread_sound(folder_name=source_folder,
+                                          cents=base_radius,
+                                          part=Decomposition.OCTAVE)
+        base_octave = base_octave * octave_portion
+        for octave_radius in combinations[Decomposition.OCTAVE]:
+            logging.debug(f"base radius: {base_radius}, octave radius {octave_radius}")
+            alt_octave, _ = get_spread_sound(folder_name=source_folder,
+                                             cents=octave_radius,
+                                             part=Decomposition.OCTAVE)
+            combination_wave = base_hollow + base_octave/2 + alt_octave
+            _custom_write(path=os.path.join(folder_path,
+                                            get_combination_naming(base_radius=base_radius,
+                                                               octave_radius=octave_radius,
+                                                               ratio=octave_portion)),
+                          sr=sr,
+                          wave=combination_wave,
+                          overwrite=overwrite)
+
 
 def tone_pair_osc_trumpet(
     cents: int,
@@ -291,7 +340,7 @@ def tone_pair_osc_trumpet(
     folder_name: str = "pairs",
     clear_dir: bool = True
 ):
-    folder_path = os.path.join("created_samples",
+    folder_path = os.path.join(OUTPUT_DIR,
                                folder_name)
     if os.path.exists(folder_path):
         if clear_dir:
@@ -323,9 +372,27 @@ if __name__ == "__main__":
     #     # os.path.pardir,
     #     "external_samples",
     #     "violin_solo.wav"))
-    create_radii(radius=50, folder_path=os.path.join("created_samples", "radius_50"))
+
+    create_radii(radius=20,
+                 folder_path=os.path.join(OUTPUT_DIR, "radius_50"),
+                 parts=[Decomposition.HOLLOW],
+                 overwrite=True
+                 )
+
+    make_combinations(folder_path=os.path.join(OUTPUT_DIR, "combinations_spacing-001"),
+                      combinations={
+                          Decomposition.FULL: [0.1, 0.2, 0.3, 0.4, 0.5,
+                                               1.0, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0,
+                                               30.0, 35.0, 40.0, 45.0],
+                          Decomposition.OCTAVE: [0.1, 0.2, 0.3, 0.4, 0.5,
+                                               1.0, 2.0, 3.0, 5.0, 10.0, 15.0, 20.0, 25.0,
+                                               30.0, 35.0, 40.0, 45.0],
+                      },
+                      source_folder="radius_50",
+                      overwrite=True,)
+
     # test, sr = trumpet_sound(frequency=440.1, bitrate=44100, plot=False, normalize=False)
-    # write(os.path.join("created_samples", "trumpet_pure_440+1.wav"), sr, test)
+    # write(os.path.join(OUTPUT_DIR, "trumpet_pure_440+1.wav"), sr, test)
     # tone_pair_osc_trumpet(cents=0, clear_dir=True)
     # for i in range(1, 20):
     #     tone_pair_osc_trumpet(cents=i/10.0, clear_dir=False)
@@ -336,7 +403,7 @@ if __name__ == "__main__":
             midi_number = librosa.hz_to_midi(440) + (i/100)
             print(f"midi number: {midi_number}")
             trumpet_missing_octave(frequency=librosa.midi_to_hz(midi_number),
-                                   file_path=os.path.join("created_samples", "cents"))
+                                   file_path=os.path.join(OUTPUT_DIR, "cents"))
     # old_generation()
     # D = np.abs(librosa.stft(audio, n_fft))
     # plt.figure()
