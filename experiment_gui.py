665e5dbaefb68bd8bd487a78f8562ff39c316668
tyfurrier <tyler.furrier@atscale.com>
2023-11-30 15:09:40 -0500

    checkpoint

--- experiment_gui.py
+++ experiment_gui.py
@@ -1,6 +1,52 @@
 import PySimpleGUI as sg
 from pygame import mixer, time
 import pygame
+import pyaudio
+import wave
+import sys
+
+def play(file_path: str):
+    # length of data to read.
+    chunk = 1024
+
+    # validation. If a wave file hasn't been specified, exit.
+
+    '''
+    ************************************************************************
+          This is the start of the "minimum needed to read a wave"
+    ************************************************************************
+    '''
+    # open the file for reading.
+    wf = wave.open(sys.argv[1], 'rb')
+
+    # create an audio object
+    p = pyaudio.PyAudio()
+
+    # open stream based on the wave object which has been input.
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    # read data (based on the chunk size)
+    data = wf.readframes(chunk)
+
+    # play stream (looping from beginning of file to the end)
+    while data:
+        # writing to the stream is what *actually* plays the sound.
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+
+    # cleanup stuff.
+    wf.close()
+    stream.close()
+    p.terminate()
+
+
+
+mixer.init()
+is_playing = False
 
 layout= [
     [sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-SOUND_PATH-")],
@@ -15,10 +61,6 @@ layout= [
 ]
 
 audio_player_window = sg.Window('Audio Player', layout, finalize=True)
-
-mixer.init()
-is_playing = False
-
 def verify_sound_object(audio_file):
 
     if not sound_path:
@@ -28,7 +70,7 @@ pygame.init()
 
 while True:
     event, values = audio_player_window.read()
-    if event == sg.WIN_CLOSED or event=="Exit":
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
 
     sound_path = values["-SOUND_PATH-"]
